3083bf1b022abb033df57cd96d056b6dc07b7d4c
Giovane <giovaneshs@gmail.com>
2023-11-20 08:15:29 -0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -130,7 +130,7 @@ def Game(stdscr):
     'Então, entrei em minha casa e, quebrando minha rotina, comecei a ler a misteriosa \ncarta'
     )
 
-    Diario(diario1,stdscr)
+    #Diario(diario1,stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -145,7 +145,7 @@ def Game(stdscr):
     'Com esperança, Grupo SKT"\n'
     )
     
-    Carta(carta1, stdscr)
+    #Carta(carta1, stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -164,7 +164,7 @@ def Game(stdscr):
     'sociedade, mas me deixou intrigada mesmo assim…\n'
     )
 
-    Diario(diario11,stdscr)
+    #Diario(diario11,stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -187,7 +187,7 @@ def Game(stdscr):
         'Emily\n'
     )
 
-    Diario(diario12,stdscr)
+    #Diario(diario12,stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -205,7 +205,7 @@ def Game(stdscr):
         'Ao anoitecer, deito em minha cama e sigo com os vagos pensamentos. O melhor a\nse fazer é dormir…\n'
     )
     
-    Diario(diario13,stdscr)
+   # Diario(diario13,stdscr)
     
     stdscr.border()
     stdscr.refresh()
@@ -245,7 +245,7 @@ def Game(stdscr):
         'Ao chegar no estabelecimento de trabalho, eu tento fazer algo em relação ao que\nfalaram na carta, talvez se eu focar em fazer só a parte de organização… vamos\nver…\n'
     )
 
-    Diario(diario2,stdscr)
+    #Diario(diario2,stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -264,7 +264,7 @@ def Game(stdscr):
         'Chegando em casa me deparei com outra carta, ansiosamente, adentrei a minha\nresidência e comecei a ler…\n'
     )
 
-    Diario(diario22,stdscr)
+    #Diario(diario22,stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -282,7 +282,7 @@ def Game(stdscr):
         'Grupo SKT"\n'
     )
 
-    Carta(carta2, stdscr)
+    #Carta(carta2, stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -305,7 +305,7 @@ def Game(stdscr):
         'uma resposta para eles.\n'
     )
 
-    Diario(diario23,stdscr)
+    #Diario(diario23,stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -338,7 +338,7 @@ def Game(stdscr):
     )
 
 
-    Diario(diario24,stdscr)
+    #Diario(diario24,stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -382,7 +382,7 @@ def Game(stdscr):
         'Hora de ir trabalhar.\n'
     )
 
-    Diario(diario3,stdscr)
+    #Diario(diario3,stdscr)
     stdscr.clear()
     stdscr.border()
     stdscr.refresh()
@@ -424,7 +424,7 @@ def Game(stdscr):
         'Em casa, recebi mais uma carta do grupo...\n'
     )
 
-    Diario(diario31,stdscr)
+    #Diario(diario31,stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -459,7 +459,7 @@ def Game(stdscr):
         'Grupo SKT"\n'
     )
 
-    Carta(carta3, stdscr)
+    #Carta(carta3, stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -478,7 +478,7 @@ def Game(stdscr):
         'que agora acredito que consiga dormir de uma melhor forma.\n'
     )
 
-    Diario(diario32, stdscr)
+    #Diario(diario32, stdscr)
     stdscr.border()
     mensagem = curses.newwin(1, 15, 14, 50)
     stdscr.refresh()
@@ -520,7 +520,7 @@ def Game(stdscr):
         'Emily"\n'
     )
 
-    Diario(diario33, stdscr)
+    #Diario(diario33, stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -565,7 +565,7 @@ def Game(stdscr):
         'confesso que era o esperado…\n\n'
     )
 
-    Diario(diario4, stdscr)
+    #Diario(diario4, stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -606,7 +606,7 @@ def Game(stdscr):
         'tinha mais outro dia a frente.\n'
     )
 
-    Diario(diario41, stdscr)
+    #Diario(diario41, stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -648,7 +648,7 @@ def Game(stdscr):
         'Faz algum tempo que não recebo mais cartas… Bem, hora de se arrumar e ir trabalhar.\n'
     )
 
-    Diario(diario5, stdscr)
+    #Diario(diario5, stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -687,7 +687,7 @@ def Game(stdscr):
         'Agora é descansar e se preparar para amanhã.\n'
     )
 
-    Diario(diario51, stdscr)
+    #Diario(diario51, stdscr)
     stdscr.border()
     stdscr.refresh()
 
@@ -729,21 +729,21 @@ def Game(stdscr):
 
     carta6 = (
         '“Prezada Emily,\n\n'
-        'Ficamos imensamente gratos por compartilhar sua experiência conosco e por sua'
-        'dedicação em explorar as intricadas conexões que moldam nosso cotidiano. Suas reflexões'
-        'e observações revelam um profundo entendimento da complexidade e beleza que muitas'
+        'Ficamos imensamente gratos por compartilhar sua experiência conosco e por sua\n'
+        'dedicação em explorar as intricadas conexões que moldam nosso cotidiano. Suas reflexões\n'
+        'e observações revelam um profundo entendimento da complexidade e beleza que muitas\n'
         'vezes passam despercebidas.\n\n'
-        'Entretanto, gostaríamos de informar que, por razões de segurança, nosso contato será'
-        'finalizado. Entendemos que você já possui o conhecimento suficiente para proliferar as'
+        'Entretanto, gostaríamos de informar que, por razões de segurança, nosso contato será\n'
+        'finalizado. Entendemos que você já possui o conhecimento suficiente para proliferar as\n'
         'ideias que você agora acredita, sendo nosso apoio não mais necessário.\n\n'
-        'Entendemos que sua jornada de exploração está apenas começando, e encorajamos'
-        'veementemente que continue a buscar novas experiências e percepções. Seu interesse em'
-        'compreender as divisões do trabalho e as contribuições silenciosas é inspirador,'
+        'Entendemos que sua jornada de exploração está apenas começando, e encorajamos\n'
+        'veementemente que continue a buscar novas experiências e percepções. Seu interesse em\n'
+        'compreender as divisões do trabalho e as contribuições silenciosas é inspirador,\n'
         'acreditamos que sua jornada de descoberta está apenas começando.\n\n'
-        'Por favor, sinta-se à vontade para permanecer ativa em sua busca e compartilhar suas'
-        'descobertas com aqueles ao seu redor. Há um vasto mundo de aprendizado à sua espera,'
+        'Por favor, sinta-se à vontade para permanecer ativa em sua busca e compartilhar suas\n\n'
+        'descobertas com aqueles ao seu redor. Há um vasto mundo de aprendizado à sua espera,\n'
         'e mal podemos esperar para ouvir mais sobre suas futuras explorações.\n\n'
-        'Agradecemos por sua compreensão e colaboração. Que seus caminhos de descoberta'
+        'Agradecemos por sua compreensão e colaboração. Que seus caminhos de descoberta\n'
         'sejam repletos de enriquecimento e aprendizado contínuo.\n\n'
         'Seja a diferença no seu mundo.\n\n'
         'Com estima,\n\n'
@@ -766,10 +766,10 @@ def Game(stdscr):
     time.sleep(0.5)
 
     diario61 = (
-        'Bem, devo dizer que é uma pena o meu contato com o Grupo SKT ter se encerrado,'
-        'minha visão do meu mundo nunca mais foi a mesma desde o momento que recebi a'
-        'carta, agora meu trabalho é proliferar essa visão, em busca de uma melhor'
-        'sociedade. Talvez sozinha não tenha a força para isso, mas com a união de uma'
+        'Bem, devo dizer que é uma pena o meu contato com o Grupo SKT ter se encerrado,\n'
+        'minha visão do meu mundo nunca mais foi a mesma desde o momento que recebi a\n'
+        'carta, agora meu trabalho é proliferar essa visão, em busca de uma melhor\n'
+        'sociedade. Talvez sozinha não tenha a força para isso, mas com a união de uma\n'
         'classe no futuro, teremos a força para tal.\n'
     )
 
